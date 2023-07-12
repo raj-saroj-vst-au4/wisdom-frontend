61c7aa20ce78ac6849948cbd88f75f0b77e1e279
raj-saroj-vst-au4 <raj-saroj-vst-au4>
2023-07-13 00:31:39 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [expandedNav, setExpandedNav] = useState(false);
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.replace("/login");
   };
+
+  const toggleNav = () => {
+    console.log(expandedNav);
+    setExpandedNav(!expandedNav);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light">
       <div className="container-fluid">
@@ -14,15 +22,19 @@ function Navbar() {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          // data-bs-toggle="collapse"
+          // data-bs-target="#navbarSupportedContent"
+          // aria-controls="navbarSupportedContent"
+          // aria-expanded="false"
+          // aria-label="Toggle navigation"
+          onClick={() => toggleNav()}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className={`navbar-collapse collapse ${expandedNav ? "show" : ""}`}
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item"></li>
 
@@ -33,13 +45,17 @@ function Navbar() {
                 id="navbarDropdown"
                 role="button"
                 data-bs-toggle="dropdown"
-                aria-expanded="false"
+                aria-expanded="true"
               >
                 Management
               </Link>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
-                  <Link className="dropdown-item" to="/students">
+                  <Link
+                    className="dropdown-item"
+                    onClick={() => toggleNav()}
+                    to="/students"
+                  >
                     Student Management
                   </Link>
                 </li>
@@ -47,7 +63,11 @@ function Navbar() {
                   <hr className="dropdown-divider" />
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/fees">
+                  <Link
+                    className="dropdown-item"
+                    onClick={() => toggleNav()}
+                    to="/fees"
+                  >
                     Fees Management
                   </Link>
                 </li>
